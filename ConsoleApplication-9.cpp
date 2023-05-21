af98f2fde970f9d90e4cbcadf7e3a2747e26d002
Alex Hashkov <alexander.gashkov@hys-enterprise.com>
2023-05-21 20:53:16 +0300

    Build files

--- ConsoleApplication-9.cpp
+++ ConsoleApplication-9.cpp
@@ -1,11 +1,11 @@
-#include "stdafx.h"
-#include "stdio.h"
+#include <iostream>
+
 int main()
 {
-	int a, b;
-	int c;
-	a = 10; b = 20;
-	c = a + b;
-	printf("a+b=%d\n", c);
-	return 0;
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    std::cout << "a + b = " << c << std::endl;
+
+    return 0;
 }
